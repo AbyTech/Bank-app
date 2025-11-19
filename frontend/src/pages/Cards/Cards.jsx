e722e71039ebc5a340133bcf6098c5a5d9caf5f2
AbyTech <abrahamisaacjames22@gmail.com>
2025-11-19 11:53:24 +0100

    Increased logo size and removed background color

--- frontend/src/pages/Cards/Cards.jsx
+++ frontend/src/pages/Cards/Cards.jsx
@@ -150,7 +150,7 @@ const Cards = () => {
 
                     {/* Company Logo */}
                     <div className="absolute top-4 right-4">
-                      <img src={logo} alt="Logo" className="w-12 h-12 rounded-full bg-white" />
+                      <img src={logo} alt="Logo" className="w-16 h-16 rounded-full" />
                     </div>
 
                     {/* Card Number */}
